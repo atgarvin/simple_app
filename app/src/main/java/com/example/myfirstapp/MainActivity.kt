8608b51ffd8f2026ab4b9609939181b891149e21
atgarvin <atgarvin@gmail.com>
2022-06-12 16:02:26 -0700

    color change buttons

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.myfirstapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+
 
 const val EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE"
 
@@ -22,5 +26,24 @@ class MainActivity : AppCompatActivity() {
             putExtra(EXTRA_MESSAGE, message)
         }
         startActivity(intent)
+    }
+
+    /** Called when the user taps the color button */
+    fun changeColor(view: View) {
+        val button = findViewById<Button>(view.id)
+        button.setBackgroundColor(Color.BLUE)
+
+        val color = (button.background as ColorDrawable).color
+
+        //buttonColor: ColorDrawable = button.background as ColorDrawable
+        //val color = buttonColor.color
+        /**if(color == Color.YELLOW) {
+            button.setBackgroundColor(Color.BLUE)
+        } else {
+            button.setBackgroundColor(Color.YELLOW)
+        }*/
+        /**if((button.background as ColorDrawable).color == Color.YELLOW) {
+            button.setBackgroundColor(Color.BLUE)
+        }*/
     }
 }
